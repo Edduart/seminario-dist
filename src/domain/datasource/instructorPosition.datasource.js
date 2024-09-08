e098965f40947467c512f665f2c5a8811369da5e
edduart <edduar13@gmail.com>
2024-09-08 16:02:32 -0400

    fix delete instructor position, env to string fix

--- src/domain/datasource/instructorPosition.datasource.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InstructorPositionDataSource = void 0;
-class InstructorPositionDataSource {
-}
-exports.InstructorPositionDataSource = InstructorPositionDataSource;
--- src/domain/repositories/instructorPosition.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InstructorPositionRepository = void 0;
-class InstructorPositionRepository {
-}
-exports.InstructorPositionRepository = InstructorPositionRepository;
